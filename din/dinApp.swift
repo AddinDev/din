908ce6206208d8f00210683ad47dfabb4da18193
AddinDev <addinsatria2004@gmail.com>
2021-10-04 13:30:59 +0700

    Initial Commit

---
+++ din/dinApp.swift
@@ -0,0 +1,17 @@
+//
+//  dinApp.swift
+//  din
+//
+//  Created by Addin Satria on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct dinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
