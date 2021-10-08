9acf1e0981ff0aaf3840db2ff4930fde98a93aae
AddinDev <addinsatria2004@gmail.com>
2021-10-08 09:43:56 +0700

    news api

--- din/Core/Utils/Networking/Api.swift
+++ din/Core/Utils/Networking/Api.swift
@@ -9,4 +9,6 @@ import Foundation
 
 struct Api {
   static let quranApi = "https://api.quran.com/api/v4/chapters"
+//  static let randomAyahApi =
+  static let newsApi = "https://newsapi.org/v2/everything?q=islam&sortBy=popularity&apiKey=f4fc7afbfa8d42538ff983705b707eae"
 }
